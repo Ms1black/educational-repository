1daf8a562d65c60e8b101d43565f62ef42382eb9
Ms1black <161579194+Ms1black@users.noreply.github.com>
2025-02-24 21:32:45 +0300

    feat: upd the code according to the second part of the lab

--- lab_02_01_01/main.cpp
+++ lab_02_01_01/main.cpp
@@ -2,114 +2,134 @@
 #include <random>
 #include <algorithm>
 
+const int SIZE_MATRIX = 14;
 
-int main() 
-{
-    int min_el = 100;
-    const int N = 14, M = 14;
-    int start = -6;
-    int end = 6;
-    int A[N][M];
-    int i, j;
+int i, j;
 
-    std::cout << "Исходная матрица:\n" << std::endl;
+void iPrintMatrix(int matrix[SIZE_MATRIX][SIZE_MATRIX]){
 
-
-    for (i = 0; i < N; i++) 
-    {
-        for (j = 0; j < M; j++) 
-        {
-            A[i][j] = rand() % (end - start + 1) + start;
-        }
-    }
-    
-
-    for (i = 0; i < N; i++) 
+    for (i = 0; i < SIZE_MATRIX; i++) 
     { 
-        for (j = 0; j < M; j++) {
+        for (j = 0; j < SIZE_MATRIX; j++) {
 
-            std::cout << A[i][j] << " ";
+            std::cout << matrix[i][j] << " ";
             
         } std::cout << std::endl; 
             
     }
+}
+
+int main() 
+{
+    int imin_el = 100;
+    int imax_column_el = -100;
+    int istart = -6;
+    int iend = 6;
+    int iArray[SIZE_MATRIX][SIZE_MATRIX];
+    
+
+    std::cout << "Исходная матрица:\n" << std::endl;
 
 
-    int min = A[0][0], max = A[0][0];
-    int min_i = 0, min_j = 0, max_i = 0, max_j = 0;
-
-    for (int i = 0; i < N; i++) 
+    for (i = 0; i < SIZE_MATRIX; i++) 
     {
-        for (int j = 0; j < M; j++) 
+        for (j = 0; j < SIZE_MATRIX; j++) 
         {
-            if (A[i][j] <= min) 
+            iArray[i][j] = rand() % (iend - istart + 1) + istart;
+        }
+    }
+    
+    iPrintMatrix(iArray);
+
+    std::cout << "Наибольшие эелементы четных столбцов:\n" << std::endl;
+
+    for (j = 1; j < SIZE_MATRIX; j+=2)
+    {
+        for (i = 0; i < SIZE_MATRIX; i++)
+        {
+                if (iArray[i][j] >= imax_column_el) 
+                {
+                    imax_column_el = iArray[i][j];
+                }
+        }
+        std::cout << imax_column_el << " ";
+        }
+     
+    int imin = iArray[0][0], imax = iArray[0][0];
+    int imin_i = 0, imin_j = 0, imax_i = 0, imax_j = 0;
+
+    for (int i = 0; i < SIZE_MATRIX; i++) 
+    {
+        for (int j = 0; j < SIZE_MATRIX; j++) 
+        {
+            if (iArray[i][j] <= imin) 
             {
-                min_el = std::min(min, A[i][j]);
-                min_i = i;
-                min_j = j;
+                imin_el = std::min(imin, iArray[i][j]);
+                imin_i = i;
+                imin_j = j;
             }
-            if (A[i][j] >= max) 
+            if (iArray[i][j] >= imax) 
             {
-                max = std::max(max, A[i][j]);
-                max_i = i;
-                max_j = j;
+                imax = std::max(imax, iArray[i][j]);
+                imax_i = i;
+                imax_j 
         
             }
         }         
     }
 
 
-    int row = -1, max_length = 0;
-    int left = 0, right = 0;
+    int iNumberMaxRow = -1, imax_length = 0;
+    int ileft = 0, iright = 0;
 
-    for (int i = 0; i < N; i++) 
+    for (int i = 0; i < SIZE_MATRIX; i++) 
     {
-        int min_pos = -1, max_pos = -1;
+        int imin_pos = -1, imax_pos = -1;
 
-        for (int j = 0; j < M; j++) 
+        for (int j = 0; j < SIZE_MATRIX;j++) 
         {
-            if (A[i][j] == min) min_pos = j;
-            if (A[i][j] == max) max_pos = j;
+            if (iArray[i][j] == imin) imin_pos = j;
+            if (iArray[i][j] == imax) imax_pos = j;
         }
 
-        if (min_pos != -1 && max_pos != -1 && min_pos != max_pos) 
+        if (imin_pos != -1 && imax_pos != -1 && imin_pos != imax_pos) 
         {
-            if (std::abs(min_pos - max_pos) - 1 > max_length) 
+            if (std::abs(imin_pos - imax_pos) - 1 > imax_length) 
                 {
-                    max_length = std::abs(min_pos - max_pos) - 1;
-                    row = i;
-                    left = std::min(min_pos, max_pos);
-                    right = std::max(min_pos, max_pos);
+                    imax_length = std::abs(imin_pos - imax_pos) - 1;
+                    iNumberMaxRow = i;
+                    ileft = std::min(imin_pos, imax_pos);
+                    iright = std::max(imin_pos, imax_pos);
                 }
 
         }
     }
 
 
-    if (row != -1) 
+    if (iNumberMaxRow != -1) 
     {
-        std::cout << "\nСтрока с самой длинной последовательностью: " << row << "\nЭлементы между min и max: ";
+        std::cout << "\nСтрока с самой длинной последовательностью: " << iNumberMaxRow << "\nЭлементы между min и max: ";
 
-        for (int j = left + 1; j < right; j++) 
+        for (int j = ileft + 1; j < iright; j++) 
         {
-            std::cout << A[row][j] << " ";
+            std::cout << iArray[iNumberMaxRow][j] << " ";
         }
     } 
     
     std::cout << std::endl;
 
 
-    for (i = 0 ; i < N; i++) 
+    for (i = 0 ; i < SIZE_MATRIX; i++) 
     {
-        for (j = 0; j < M; j++) 
+        for (j = 0; j < SIZE_MATRIX; j++) 
         {
             if(i<=j)
             {
-               if(i + j + 1 >= N)
+               if(i + j + 1 >= SIZE_MATRIX)
                {
-                    if(A[i][j] <= min_el)
+                    if(iArray[i][j] <= imin_el)
                     {
-                        min_el = A[i][j];
+                        imin_el = iArray[i][j];
                     }
                } 
             }
@@ -117,8 +137,9 @@ int main()
         }
     }
 
-    std::cout << "\nМинимальный элемент среди выделенных черным цветом: "<< min_el << std::endl;
+    std::cout << "\nМинимальный элемент среди выделенных черным цветом: "<< imin_el << std::endl;
 
 
     return 0;
+    
 }
