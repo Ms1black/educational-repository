649626fed5318505fa9d8ffdf6dcbae7cced6b9c
Liza <li304ko3@gmail.com>
2025-03-16 13:56:18 +0300

    fix: delete a problem with output

--- lab_02_01_02/Models/track_class/track.cpp
+++ lab_02_01_02/Models/track_class/track.cpp
@@ -9,29 +9,26 @@ void Track::fillInfoAboutTrack() {
     std::cout << "Введите жанр:\n";
     std::getline(std::cin, track_genre);
 
-    std::cout << "Введите количество минут:\n";
+    std::cout << "Введите исполнителя:\n";
     std::getline(std::cin, track_artist);
 
-    std::string during_track_min;
+    std::cout << "Введите количество минут:\n";
+    std::cin >> during_track_min;   
+
     std::cout << "Введите количество секунд:\n";
-    std::getline(std::cin, during_track_min);   
+    std::cin >> during_track_sec;
 
-    std::string during_track_sec;
-    std::cout << "Введите исполнителя:\n";
-    std::getline(std::cin, during_track_sec);
-
-    std::string age_limit_track;
     std::cout << "Введите возрастное ограничение:\n";
-    std::getline(std::cin, age_limit_track);
+    std::cin >> age_limit_track;
 
 }
 
 void Track::displayInfoAboutTrack() {
 
     std::cout << textalign(track_name.c_str(), LEFT, 20) 
-    << textalign(track_genre.c_str(), CENTER, 20)
-    << textalign((std::to_string(during_track_min) + ":" + std::to_string(during_track_sec)).c_str(), RIGHT, 20)
-    << textalign(track_artist.c_str(), RIGHT, 25)
+    << textalign(track_genre.c_str(), LEFT, 20)
+    << textalign(track_artist.c_str(), LEFT, 25)
+    << textalign((std::to_string(during_track_min) + ":" + std::to_string(during_track_sec)).c_str(), LEFT, 20)  
     << textalign((std::to_string(age_limit_track) + "+").c_str(), LEFT, 10) << "\n";
 
 
